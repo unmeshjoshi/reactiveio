d8b2d880159d367ef0ea5a81c838e0a8b61bbc0f
unmesh <unmeshjoshi@gmail.com>
2019-05-14 14:57:06 +0530

    adding materialization

--- src/test/scala/com/reactive/stream/impl/StreamStageMaterializationTest.scala
+++ src/test/scala/com/reactive/stream/impl/StreamStageMaterializationTest.scala
@@ -4,12 +4,26 @@ import org.scalatest.FunSuite
 
 class StreamStageMaterializationTest extends FunSuite {
 
+  def materialize(module: Module):List[(Connection, List[GraphStageLogic])] = {
+
+
+  }
+
   test("Should fuse graph stages and materialise") {
     val source = new SourceStage
     val sink = new SinkStage
 
     val module = source.module.fuse(sink.module, source.module.shape.outlets.head, sink.module.shape.inlets.head)
     println(module)
+  }
+
+  test("should form connections for connected graph stages") {
+    val source = new SourceStage
+    val sink = new SinkStage
+
+    val module = source.module.fuse(sink.module, source.module.shape.outlets.head, sink.module.shape.inlets.head)
+    val tuples = materialize(module)
+    assert(1 == tuples.size)
   }
 
 }
